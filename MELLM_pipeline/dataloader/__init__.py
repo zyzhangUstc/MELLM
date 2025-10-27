d65d102504acce3e8706c70fba276990fbd5787b
zyzhangUstc <zyzhangzyz@gmail.com>
2025-10-27 09:49:11 +0800

    Delete MELLM_pipeline/dataloader directory

--- MELLM_pipeline/dataloader/__init__.py
+++
@@ -1,8 +0,0 @@
-from dataloader.flow.chairs import FlyingChairs
-from dataloader.flow.things import FlyingThings3D
-from dataloader.flow.sintel import MpiSintel
-from dataloader.flow.kitti import KITTI
-from dataloader.flow.spring import Spring
-from dataloader.flow.hd1k import HD1K
-
-from dataloader.stereo.tartanair import TartanAir
